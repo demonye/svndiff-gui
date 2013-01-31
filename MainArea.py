be59674931ab2ce5e042eb969f5d9cfae01ced47
demonye <demon.ye@gmail.com>
2013-01-31 17:04:39 +0800

    Import backup and restore, write simple manual

--- MainArea.py
+++ MainArea.py
@@ -18,12 +18,8 @@ from yelib.util import *
 from SettingsDlg import decrypt
 
 
-hdiff_dir = os.path.join("data", "hdiff")
-backup_dir = os.path.join("data", "backup")
-clstemp_dir = os.path.join("data", "clstemp")
-approot_dir = os.path.join("data", "approot")
-target_dir = os.path.join("data", "target")
-target_file = os.path.join(target_dir, "target.jar")
+def dpath(app, path):
+    return os.path.join("data", app, path)
 
 class MainArea(QWidget):
 
@@ -31,7 +27,6 @@ class MainArea(QWidget):
         super(MainArea, self).__init__(parent)
 
         self.settings = parent.dlgSettings
-        self.jar_bin = self.settings.conf('app', 'jar bin')
 
         # ==== File List ====
         # Application Combo
@@ -136,6 +131,7 @@ class MainArea(QWidget):
         self.btnGetStatus.clicked.connect(self.getStatus)
         self.btnMakeDiff.clicked.connect(self.makeDiff)
         self.btnDeployNew.clicked.connect(self.deployNew)
+        self.btnRestore.clicked.connect(self.restoreFiles)
         self.workdir = os.getcwdu()
         self.targetFile = None
 
@@ -216,7 +212,7 @@ class MainArea(QWidget):
 
     def getStatusHandler(self, msg):
         if msg.type != OutputType.OUTPUT:
-        	return
+            return
         tb = self.lstFiles
         m = msg.output.split()
         if m[0] not in self.statusIcons:
@@ -236,6 +232,10 @@ class MainArea(QWidget):
         tb.setItem(n, 4, QTableWidgetItem(m[2]))
 
     def makeDiff(self):
+        st = self.settings
+        app = self.cboApp.itemText(self.cboApp.currentIndex())
+        hdiff = dpath(app, 'hdiff')
+
         def begin():
             self.btnMakeDiff.setDisabled(True)
             self.parent().showLoading(u'Making diff files ... ', True)
@@ -255,7 +255,7 @@ class MainArea(QWidget):
                 files.append(tb.item(i, 4).text())
         cmds = [
                 os.path.join("bin", "svndiff"),
-                "-s", srcdir, "-d", hdiff_dir,
+                "-s", srcdir, "-d", hdiff,
                 "-f", os.path.join("bin", "diff"),
                 "-v", os.path.join("bin", "svn"),
                 "-t", os.path.join("html", "diff_template.html"),
@@ -267,7 +267,7 @@ class MainArea(QWidget):
                 TaskHandler(self.defaultHandler)
                 )
 
-        path = os.path.join(os.getcwdu(), hdiff_dir).replace(os.sep, '/')
+        path = os.path.join(os.getcwdu(), hdiff).replace(os.sep, '/')
         task.put(self.echoMsg(
                 u"Go to <a href='{}' style='color:dodgerblue;'>HDIFF Directory</a> "
                 "to check the result.".format(path)
@@ -300,11 +300,15 @@ class MainArea(QWidget):
         self.worker.add_task(task)
 
     def _uploadDiffs(self, dstdir, sshargs):
+        st = self.settings
+        app = self.cboApp.itemText(self.cboApp.currentIndex())
+        hdiff = dpath(app, 'hdiff')
+
         sshcli = SSHClient()
         sftpcli = None
         code = 0
         try:
-            if not (yield TaskOutput(u'Conntecting to %s ...' % sshargs['hostname'])):
+            if not (yield TaskOutput(u'Connecting to %s ...' % sshargs['hostname'])):
                 raise CommandTerminated()
             sshcli.set_missing_host_key_policy(AutoAddPolicy())
             sshcli.connect(**sshargs)
@@ -315,9 +319,9 @@ class MainArea(QWidget):
             if errstr != '':
                 raise Exception(errstr)
             sftpcli = sshcli.open_sftp()
-            for f in os.listdir(hdiff_dir):
+            for f in os.listdir(hdiff):
                 if f.lower().endswith('.html'):
-                    localfile = os.path.join(hdiff_dir, f)
+                    localfile = os.path.join(hdiff, f)
                     remotefile = os.path.join(dstdir, f).replace(os.sep, '/')
                     if not (yield TaskOutput(u'Uploading %s ...' % f)):
                         raise CommandTerminated()
@@ -335,19 +339,25 @@ class MainArea(QWidget):
 
 
     def _copyfile(self, ssh, sftp, from_file, to_file, method='get', overwrite=True):
-        if os.path.isdir(to_file):
-            fname = os.path.basename(from_file)
-            to_dir = to_file
-            to_file = os.path.join(to_file, fname)
-        else:
-            to_dir = os.path.dirname(to_file)
-        if not overwrite and os.path.exists(to_file):
-            return
-        if not os.path.exists(to_dir):
-            mkdir_p(to_dir)
+        if not sftp or method == 'get':
+            if os.path.isdir(to_file):
+                fname = os.path.basename(from_file)
+                to_dir = to_file
+                to_file = os.path.join(to_file, fname)
+            else:
+                to_dir = os.path.dirname(to_file)
+            if not overwrite and os.path.exists(to_file):
+                return
+            if not os.path.exists(to_dir):
+                mkdir_p(to_dir)
 
         if sftp:
             if method == 'put':
+                to_dir = os.path.dirname(to_file)
+            	_,stdout,stderr = ssh.exec_command('mkdir -p ' + to_dir)
+            	stdout.read()
+            	errmsg = stderr.read()
+                if errmsg: raise Exception(errmsg)
                 sftp.put(from_file, to_file)
             else:
                 sftp.get(from_file, to_file)
@@ -377,21 +387,21 @@ class MainArea(QWidget):
             self._copyfile(ssh, sftp, files[0], to_dir, method, overwrite)
 
 
-# data/backup/appname/
-#    |_ other files 
-#    |_ WEB-INF/classes/com/.../*.class
-#    \_ WEB-INF/lib/*.jar
-# data/approot  ----------- Stores all new stuff to be deployed
-#    |_ other files 
-#    |_ WEB-INF/classes/com/.../*.class
-#    \_ WEB-INF/lib/*.jar
-# data/clstemp  ----------- new classes to be put in jar
-#    \_ com/.../*.class
+    # data/appname
+    #   backup
+    #    |_ other files 
+    #    |_ WEB-INF/classes/com/.../*.class
+    #    \_ WEB-INF/lib/*.jar
+    #   approot  ----------- Stores all new stuff to be deployed
+    #    |_ other files 
+    #    |_ WEB-INF/classes/com/.../*.class
+    #    \_ WEB-INF/lib/*.jar
+    #   clstemp  ----------- new classes to be put in jar
+    #    \_ com/.../*.class
     def deployNew(self):
         st = self.settings
         app = self.cboApp.itemText(self.cboApp.currentIndex())
         sshcli = SSHClient()
-        sftpcli = None
 
         hostname = st.conf(app, 'server')
         sshargs = {}
@@ -407,31 +417,24 @@ class MainArea(QWidget):
         def begin():
             self.btnDeployNew.setDisabled(True)
             self.parent().showLoading(u'Deploying Target File ... ', True)
-            if sshargs:
-                try:
-                    self.appendLog(TaskOutput(u'Conntecting to %s ...' % sshargs['hostname']))
-                    sshcli.set_missing_host_key_policy(AutoAddPolicy())
-                    sshcli.connect(**sshargs)
-                    self.appendLog(TaskOutput(u'Connected'))
-                    sftpcli = sshcli.open_sftp()
-                except Exception as ex:
-                    self.appendLog(repr(ex), OutputType.ERROR)
+
         def end():
-            if sftpcli: sftpcli.close()
             if sshcli: sshcli.close()
             self.parent().showLoading('', False)
             self.btnDeployNew.setDisabled(False)
 
         try:
-            force_rmdir(approot_dir)
-            force_rmdir(clstemp_dir)
-            mkdir_p(approot_dir)
-            mkdir_p(clstemp_dir)
+            clstemp = dpath(app, 'clstemp')
+            approot = dpath(app, 'approot')
+            force_rmdir(approot)
+            force_rmdir(clstemp)
+            mkdir_p(approot)
+            mkdir_p(clstemp)
         except Exception as ex:
             self.appendLog(TaskOutput(ex.message, OutputType.ERROR))
             return
 
-        task = Task(self._deployNew(st, app, sshcli, sftpcli))
+        task = Task(self._deployNew(st, app, sshargs, sshcli))
         task.init(
                 TaskHandler(begin), TaskHandler(end),
                 TaskHandler(self.defaultHandler),
@@ -446,7 +449,7 @@ class MainArea(QWidget):
 
 
     # Fetch and backup files
-    def _deployNew(self, st, app, sshcli, sftpcli):
+    def _deployNew(self, st, app, sshargs, sshcli):
         tb = self.lstFiles
         code = 0
         srcroot = st.conf(app, 'source root')
@@ -456,11 +459,19 @@ class MainArea(QWidget):
         tgroot = st.conf(app, 'target root')
         tgsep = '/'
         tgclsdir = st.conf(app, 'target class dir')
-        appbakdir = os.path.join(backup_dir, app)
+        bakdir = dpath(app, 'backup')
+        approot = dpath(app, 'approot')
         clsitems = []  # items found in class dir or non-java-file
-        jaritems = {}   # items found in jar file
+        sftpcli = None
 
         try:
+            if sshargs:
+                self.appendLog(TaskOutput(u'Conntecting to %s ...' % sshargs['hostname']))
+                sshcli.set_missing_host_key_policy(AutoAddPolicy())
+                sshcli.connect(**sshargs)
+                sftpcli = sshcli.open_sftp()
+                self.appendLog(TaskOutput(u'Connected'))
+
             items = [ tb.item(i,3).text() for i in xrange(tb.rowCount())
                     if tb.item(i,0).checkState() == Qt.Checked ]
 
@@ -468,29 +479,31 @@ class MainArea(QWidget):
             # backup class files, and copy them to approot dir
             if not (yield TaskOutput(u'Fetching, making backup and copying files ... ')):
                 raise CommandTerminated()
+
             for fn in items:
                 if fn.endswith('.java'):
                     fn = fn.replace(javadir.replace(os.sep, '/'), '').lstrip('/')
                     _clsfn = fn[0:-5]+'.class'
                     clsfn = os.path.join(tgclsdir, _clsfn).replace(os.sep, tgsep)
                     tgt_f = os.path.join(tgroot, clsfn).replace(os.sep, tgsep)
-                    bak_f = os.path.join(appbakdir, clsfn)
+                    bak_f = os.path.join(bakdir, clsfn)
                     new_f = os.path.join(srcroot, clsdir, _clsfn)
-                    tmp_f = os.path.join(approot_dir, clsfn)
+                    tmp_f = os.path.join(approot, clsfn)
                     clsitems.append(_clsfn)
                 else:
                     fn = fn.replace(webappdir.replace(os.sep, '/'), '').lstrip('/')
                     tgt_f = os.path.join(tgroot, fn).replace(os.sep, tgsep)
-                    bak_f = os.path.join(appbakdir, fn)
+                    bak_f = os.path.join(bakdir, fn)
                     new_f = os.path.join(srcroot, webappdir, fn)
-                    tmp_f = os.path.join(approot_dir, fn)
+                    tmp_f = os.path.join(approot, fn)
                 try:
-                    self._copyfile(sshcli, sftpcli, tgt_f, bak_f, overwrite=False)
+                    overwrite = (self.chkBackup.checkState() == Qt.Checked and True or False)
+                    self._copyfile(sshcli, sftpcli, tgt_f, bak_f, overwrite=overwrite)
                     self._copyfile(None, None, new_f, tmp_f)
                 except IOError as ex:   # No file found
                     if ex.args[0] != 2:
                         raise ex
-                if not (yield TaskOutput(u'Copying files ... ')):
+                if not (yield TaskOutput(u'Copying %s ... ' % tgt_f)):
                     raise CommandTerminated()
             self.worker.add_step(self._updateNew(st, app, sshcli, sftpcli, clsitems))
         except CommandTerminated:
@@ -510,20 +523,24 @@ class MainArea(QWidget):
         srcroot = st.conf(app, 'source root')
         clsdir = st.conf(app, 'class dir')
         tgroot = st.conf(app, 'target root')
-        appbakdir = os.path.join(backup_dir, app)
+        bakdir = dpath(app, 'backup')
+        clstemp = dpath(app, 'clstemp')
+        approot = dpath(app, 'approot')
+        tgclsdir = st.conf(app, 'target class dir')
         tgjarfile = st.conf(app, 'target jar file')
         jardir = os.path.dirname(tgjarfile)
-        bakjardir = os.path.join(appbakdir, jardir)
-        newjardir = os.path.join(approot_dir, jardir)
+        bakjardir = os.path.join(bakdir, jardir)
+        newjardir = os.path.join(approot, jardir)
         tgsep = '/'
         jaritems = {}
 
         try:
+            overwrite = (self.chkBackup.checkState() == Qt.Checked and True or False)
             self._copyfiles(sshcli, sftpcli,
                     os.path.join(tgroot, tgjarfile).replace(os.sep, tgsep),
-                    bakjardir, overwrite=False)
+                    bakjardir, overwrite=overwrite)
             self._copyfiles(None, None,
-                    os.path.join(appbakdir, tgjarfile),
+                    os.path.join(bakdir, tgjarfile),
                     newjardir)
 
             if not (yield TaskOutput(u'Copying new class files ... ')):
@@ -536,7 +553,7 @@ class MainArea(QWidget):
                 for fn in clsitems:
                     if fn in zflist:
                         new_f = os.path.join(srcroot, clsdir, fn)
-                        tmp_f = os.path.join(clstemp_dir, fn)
+                        tmp_f = os.path.join(clstemp, fn)
                         self._copyfile(None, None, new_f, tmp_f)
                         jaritems[jar].append(fn)    # clsss in which jarfile
                         (yield TaskOutput((jar, fn), OutputType.OUTPUT))
@@ -546,11 +563,25 @@ class MainArea(QWidget):
             for jar, files in jaritems.items():
                 if len(files) == 0:
                     continue
-                args = [ os.path.join(self.jar_bin), "-uf",
-                        os.path.join("..", "..", newjardir, jar)
+                jar_bin = self.settings.conf('app', 'jar bin')
+                args = [ os.path.join(jar_bin), "-uf",
+                        os.path.join("../approot", jardir, jar)
                        ] + files
-                self.worker.add_step(CmdTask(args, clstemp_dir))
-            self.worker.add_step(self._uploadNew(st, app, sshcli, sftpcli))
+                self.worker.add_step(CmdTask(args, clstemp))
+
+            for fn in clsitems:
+            	notjaritem = True
+                for v in jaritems.values():
+                    if fn in v:
+                    	notjaritem = False
+                    	break
+                if notjaritem:
+                	self._copyfile(None, None,
+                	        os.path.join(srcroot, clsdir, fn),
+                	        os.path.join(approot, tgclsdir, fn)
+                	        )
+
+            self.worker.add_step(self._uploadNew(st, app, sshcli, sftpcli, 'approot'))
         except CommandTerminated:
             code = -2
             (yield TaskOutput(u'Updating Terminited', OutputType.WARN))
@@ -561,23 +592,100 @@ class MainArea(QWidget):
             (yield TaskOutput(u'EXIT %d' % code, OutputType.NOTIFY))
 
 
-    def _uploadNew(self, st, app, sshcli, sftpcli):
+    def _uploadNew(self, st, app, sshcli, sftpcli, rootpath):
         tgroot = st.conf(app, 'target root')
+        uproot = dpath(app, rootpath)
         tgsep = '/'
         code = 0
+        startcmd = st.conf(app, 'startup')
+        stopcmd = st.conf(app, 'shutdown')
         try:
-            for r,_,files in os.walk(approot_dir):
+            if stopcmd:
+                if not (yield TaskOutput(u'Stopping service ... ')):
+                    raise CommandTerminated()
+                if sftpcli:
+                    _,stdout,stderr = sshcli.exec_command(stopcmd)
+                    stdout.read()
+                    errmsg = stderr.read()
+                    if errmsg:
+                        raise Exception(errmsg)
+                else:
+                    os.system(stopcmd)
+            for r,_,files in os.walk(uproot):
                 for f in files:
                     srcfile = os.path.join(r, f)
-                    tgfile = os.path.join(tgroot, r.replace(approot_dir+os.sep, ''),
-                            f).replace(os.sep, tgsep)
+                    root = r.replace(uproot, '').lstrip(os.sep)
+                    tgfile = os.path.join(tgroot, root, f).replace(os.sep, tgsep)
                     if not (yield TaskOutput(u'Uploading %s ... ' % srcfile)):
                         raise CommandTerminated()
                     self._copyfile(sshcli, sftpcli, srcfile, tgfile, method='put')
+            if startcmd:
+                if not (yield TaskOutput(u'Starting service ... ')):
+                    raise CommandTerminated()
+                if sftpcli:
+                    sshcli.exec_command(startcmd)
+                    stdout.read()
+                    errmsg = stderr.read()
+                    if errmsg:
+                        raise Exception(errmsg)
+                else:
+                    os.system(startcmd)
             (yield TaskOutput(u'Files Uploaded'))
         except CommandTerminated:
             code = -2
             (yield TaskOutput(u'Uploading Terminited', OutputType.WARN))
+        except Exception as ex:
+            code = -1
+            (yield TaskOutput(repr(ex), OutputType.ERROR))
+        finally:
+            (yield TaskOutput(u'EXIT %d' % code, OutputType.NOTIFY))
+
+    def restoreFiles(self):
+        sshcli = SSHClient()
+
+        def begin():
+            self.btnRestore.setDisabled(True)
+            self.parent().showLoading(u'Restoring files ... ', True)
+
+        def end():
+            if sshcli: sshcli.close()
+            self.btnRestore.setDisabled(False)
+            self.parent().showLoading('', False)
+
+        task = Task(self._restoreFiles(sshcli))
+        task.init(
+                TaskHandler(begin), TaskHandler(end),
+                TaskHandler(self.defaultHandler),
+                )
+        self.worker.add_task(task)
+
+    def _restoreFiles(self, sshcli):
+        st = self.settings
+        app = self.cboApp.itemText(self.cboApp.currentIndex())
+        code = 0
+        bakdir = dpath(app, 'backup')
+        hostname = st.conf(app, 'server')
+        sftpcli = None
+        sshargs = {}
+        if hostname not in ('localhost', '127.0.0.1'):
+            sshargs = {
+                'hostname': hostname,
+                'username': st.conf(app, 'username'),
+                'password': decrypt(st.conf(app, 'password')),
+                'timeout' : 10,
+                'compress': True,
+                }
+
+        try:
+            if sshargs:
+                if not (yield TaskOutput(u'Connecting to %s ...' % sshargs['hostname'])):
+                    raise CommandTerminated()
+                sshcli.set_missing_host_key_policy(AutoAddPolicy())
+                sshcli.connect(**sshargs)
+                if not (yield TaskOutput(u'Connected, ready to upload ...')):
+                    raise CommandTerminated()
+                sftpcli = sshcli.open_sftp()
+            self.worker.add_step(self._uploadNew(st, app, sshcli, sftpcli, 'backup'))
         except Exception as ex:
             code = -1
             (yield TaskOutput(repr(ex), OutputType.ERROR))
--- svndiff-gui.py
+++ svndiff-gui.py
@@ -4,54 +4,109 @@
 import sys, os
 from PySide.QtCore import *
 from PySide.QtGui import *
+from paramiko import SSHClient, AutoAddPolicy
+
 from yelib.qt.layout import *
-import cPickle as pk
-from tabs import *
-from yelib.newtask import TaskWorker
+from yelib.task import *
+#from tabs.BaseTab import BaseTab, IconLabel
+
+from SettingsDlg import SettingsDlg
+from MainArea import MainArea
 
 __version__ = "1.1"
+
+STYLE_SHEET = """
+/*
+QTabBar::tab,
+*/
+QPushButton {
+    border: 1px outset rgb(150,150,150);
+    border-radius: 3px;
+}
+QPushButton:hover {
+	border: 1.5px outset dimgray;
+    background: rgb(150,150,150);
+}
+/*
+QTabBar::tab:hover { background: rgb(150,150,150); }
+QTabBar::tab:selected {
+    color: rgb(220,220,220);
+    background: rgb(130,130,130);
+    border: 1px inset rgb(150,150,150);
+}
+QTabWidget::pane { border:1px solid rgb(150,150,150);}
+*/
+QStatusBar::item { border:0; }
+QLineEdit, QTextEdit, QComboBox,
+QListWidget, QTableWidget, QGroupBox {
+    padding: 2px; border-radius: 3px;
+    border:1px solid rgb(150,150,150);
+}
+QLineEdit:focus, QTextEdit:focus {
+    border:1.5px solid goldenrod;
+}
+QGroupBox{ margin-top: 5px; }
+QGroupBox::title { left: 10px; top: -5px; }
+QListWidget, QTableWidget { padding: 0; }
+QListWidget::item { margin:2px; }
+QListWidget::item QLineEdit:focus { border:0; border-radius:0; margin:0; padding:0; }
+QHeaderView::section {
+    padding: 3px;
+    border: 1px solid rgb(150,150,150);
+}
+QTextBrowser {
+    color: white;
+    /* background:lightyellow; */
+    background: rgb(50,50,50);
+    padding: 5px; border: 0;
+}
+IconLabel { border:0; }
+/*
+IconLabel:hover { background:none; }
+*/
+"""
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__() 
 
+        self.setFont(QFont("Monospace", 10))
+        self.dlgSettings = SettingsDlg(self)
+        self.dlgSettings.setModal(True)
+        self.dlgSettings.load()
         self.main = MainArea(self)
         self.setCentralWidget(self.main)
-        self.dlgSettings = SettingsTab(self)
-        for i in xrange(self.main.tab.count()):
-            w = self.main.tab.widget(i)
-            w.init(self.dlgSettings)
 
         self.createToolBar()
         self.createStatusBar()
 
         self.setWindowTitle('Svn Tool')
-        self.setWindowIcon(QIcon('logo.png'))
+        self.setWindowIcon(QIcon('image/logo.png'))
+        self.setStyleSheet(STYLE_SHEET)
 
     def createStatusBar(self):
         self.lbLoadingText = QLabel()
         self.lbLoadingGif = QLabel()
         self.lbLoadingGif.hide()
-        movie = QMovie("loading-small.gif")
+        movie = QMovie("image/loading.gif")
         movie.start()
         self.lbLoadingGif.setMovie(movie)
 
         self.statusBar()
-        self.statusBar().addPermanentWidget(self.lbLoadingGif)
-        self.statusBar().addPermanentWidget(self.lbLoadingText)
-        self.setStyleSheet("QStatusBar::item {border-style:flat;}")
+        self.statusBar().addWidget(self.lbLoadingText)
+        self.statusBar().addWidget(self.lbLoadingGif)
 
     def createToolBar(self):
         self.myTb = self.addToolBar("Svn Tool")
         self.myTb.addAction( QAction(
-            QIcon('stop-task.png'), "Stop Current Running Task", self,
-            # statusTip="Stop current running task",
-            triggered=self.stopCurrentTask) )
-        self.myTb.addAction( QAction(
-            QIcon('settings.png'), "Open Settings Dialog", self,
-            # statusTip="Open Settings Dialog",
+            QIcon('image/computer.png'), "Open Settings Dialog", self,
+            #statusTip="Open Settings Dialog",
             triggered=self.openSettingsTab) )
+        self.myTb.addAction( QAction(
+            QIcon('image/stop.png'), "Stop Current Running Task", self,
+            #statusTip="Stop Current Running Task",
+            triggered=self.stopCurrentTask) )
 
     def stopCurrentTask(self):
         TaskWorker().stop_task()
@@ -65,7 +120,7 @@ class MainWindow(QMainWindow):
             self.rect().center() )
 
     def closeEvent(self, event):
-        self.dlgSettings.saveConfig()
+        #self.dlgSettings.saveConfig()
         self.main.close()
         event.accept()
 
@@ -74,122 +129,9 @@ class MainWindow(QMainWindow):
         #self.statusBar().showMessage(msg)
         self.lbLoadingGif.setVisible(loading)
 
-class MainArea(QWidget):
+    def notifyConfigChanged(self):
+        self.main.updateAppList()
 
-    def __init__(self, parent=None):
-        super(MainArea, self).__init__(parent)
-        self.setFont(QFont("Monospace", 10))
-
-        # ==== Tab Widget ====
-        self.tab = QTabWidget()
-        self.tabDiff = DiffTab(self)
-        self.tabClass = ClassTab(self)
-        self.tab.addTab(self.tabDiff, u'Make Diff')
-        self.tab.addTab(self.tabClass, u'Replace Class File')
-        self.tab.setMinimumSize(700, 400)
-        self.tab.setSizePolicy(
-            QSizePolicy.Policy(QSizePolicy.Preferred),
-            QSizePolicy.Policy(QSizePolicy.Maximum)
-            )
-        # ==== Tab Widget ====
-
-        # ==== Log ====
-        self.grpLog = QGroupBox(u'Information of Execution')
-        self.txtLog = QTextBrowser()
-        self.txtLog.setReadOnly(True)
-        self.txtLog.setMinimumHeight(150)
-        self.txtLog.setOpenExternalLinks(True)
-        self.ltLog = yBoxLayout([
-            [ self.txtLog ],
-        ])
-        self.grpLog.setLayout(self.ltLog)
-        self.tab.setSizePolicy(
-            QSizePolicy.Policy(QSizePolicy.Preferred),
-            QSizePolicy.Policy(QSizePolicy.Minimum)
-            )
-        # ==== Log ====
-        self.tab.setCurrentIndex(1)
-
-        # ==== Main Layout ====
-        splitter = QSplitter(self)
-        splitter.setOrientation(Qt.Vertical)
-        splitter.setChildrenCollapsible(False)
-        splitter.addWidget(self.tab)
-        splitter.addWidget(self.grpLog)
-        #self.btnExit = QPushButton(u'Exit')
-        self.lt = yBoxLayout([
-            [ splitter ],
-        ])
-        #self.btnExit.clicked.connect(self.close)
-        self.setLayout(self.lt)
-        if True:
-            self.setStyleSheet("""
-            /*
-            QLabel {
-                padding:2px;
-                border:1px inset rgb(150,150,150);
-                border-radius:3px;
-                }
-            */
-            /*QTabBar::tab, */
-            QPushButton {
-                padding: 5px;
-                /*background: rgb(180,180,180);*/
-                border: 1px solid rgb(150,150,150);
-                border-radius: 2px;
-            }
-            /*
-            QTabBar::tab:hover { background: rgb(150,150,150); }
-            QTabBar::tab:selected {
-                color: rgb(220,220,220);
-                background: rgb(130,130,130);
-                border: 1px inset rgb(150,150,150);
-            }
-            QTabWidget::pane { border:1px solid rgb(150,150,150);}
-            */
-            QPushButton:hover { background: rgb(150,150,150); }
-            QLineEdit, QComboBox, QTableWidget, QGroupBox {
-                padding: 2px; border-radius: 3px;
-                border:1px solid rgb(150,150,150);
-            }
-            QGroupBox{ margin-top: 5px; }
-            QGroupBox::title { left: 10px; top: -5px; }
-            QTableWidget { padding: 0; }
-            QHeaderView::section {
-                padding: 3px;
-                border: 1px solid rgb(150,150,150);
-            }
-            QTextBrowser {
-                color: white;
-                /* background:lightyellow; */
-                background: rgb(50,50,50);
-                padding: 5px; border: 0;
-            }
-            """ )
-        # ==== Main Layout ====
-
-    def center(self):
-        self.move(
-            QApplication.desktop().screen().rect().center() -
-            self.rect().center() )
-
-    #def reject(self):
-    #    self.close()
-
-    def closeEvent(self, event):
-        for i in xrange(self.tab.count()):
-            self.tab.widget(i).close()
-        event.accept()
-
-    def append_log(self, logtext=''):
-        self.txtLog.append(logtext)
-
-    def tab_changed(self, idx):
-        pass
-        #if self.tab.currentWidget() == self.tabSettings:
-        #	self.grpLog.hide()
-        #else:
-        #	self.grpLog.show()
  
 if __name__ == "__main__":
     app = QApplication(sys.argv)
